71919a0694b03c7286492fbed11cb786029251ad
Rofiyev Dilshod <rofiyevdilshod@gmail.com>
2023-12-14 12:57:36 +0500

    ......

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -4,23 +4,25 @@
       <h4 class="my-0 mr-md-auto fw-bold">R.D.7</h4></RouterLink
     >
 
-    <nav v-if="!user" class="d-inline-flex ms-md-auto">
-      <RouterLink :to="{ name: 'login' }" class="me-3 py-2 text-dark text-decoration-none"
-        >Login</RouterLink
-      >
-      <RouterLink :to="{ name: 'register' }" class="me-3 py-2 text-dark text-decoration-none"
-        >Register</RouterLink
-      >
-    </nav>
-    <nav v-else class="d-inline-flex ms-md-auto">
-      <div class="d-flex gap-2">
-        <img class="rounded-circle" height="50" width="50" :src="user.image" alt="user" />
+    <nav class="d-inline-flex ms-md-auto">
+      <template v-if="!user">
+        <RouterLink :to="{ name: 'login' }" class="me-3 py-2 text-dark text-decoration-none"
+          >Login</RouterLink
+        >
+        <RouterLink :to="{ name: 'register' }" class="me-3 py-2 text-dark text-decoration-none"
+          >Register</RouterLink
+        >
+      </template>
+      <template v-else>
+        <div class="d-flex gap-2">
+          <img class="rounded-circle" height="50" width="50" :src="user.image" alt="user" />
 
-        <div>
-          <h5 class="mb-0">{{ user.username }}</h5>
-          <span>{{ user.email }}</span>
+          <div>
+            <h5 class="mb-0">{{ user.username }}</h5>
+            <span>{{ user.email }}</span>
+          </div>
         </div>
-      </div>
+      </template>
     </nav>
   </div>
 </template>
