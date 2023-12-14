4ca8661ff843d3f0f2765e2fdc400c2033fffefd
Rofiyev Dilshod <rofiyevdilshod@gmail.com>
2023-12-14 13:00:09 +0500

    ......

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -3,7 +3,6 @@
     <RouterLink :to="{ name: 'home' }" class="text-dark text-decoration-none">
       <h4 class="my-0 mr-md-auto fw-bold">R.D.7</h4></RouterLink
     >
-
     <nav class="d-inline-flex ms-md-auto">
       <template v-if="!user">
         <RouterLink :to="{ name: 'login' }" class="me-3 py-2 text-dark text-decoration-none"
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as NavbarComponent } from './NavbarComponent.vue'
 export { default as RegisterComponent } from './RegisterComponent.vue'
-export {default as LoginComponent} from './LoginComponent.vue'
+export { default as LoginComponent } from './LoginComponent.vue'
